e09701847fc95213ff20bd1f6b97e7c2e031d581
Taner Tun <t_tun@msn.com>
2022-01-20 15:16:24 +0200

    updated

--- app.js
+++ app.js
@@ -1,3 +1,40 @@
+const cookieStorages = {
+    getItem: (key) => {
+        const cookies = document.cookie
+        .split(';')
+        .map(cookie => cookie.split('='))
+        .reduce((acc, [key, value]) => ({...acc, [key.trim()]: value}), {});
+      return cookies[key];
+    },
+    setItem: (key, value) => {
+        document.cookie = `${key}=${value}`;
+    },
+};
+
+const storage = cookieStorages;
+const consentForm = 'user_consent';
+
+const popup = () => !storage.getItem(consentForm);
+const saveToStorage = () => storage.setItem(consentForm, true);
+
+window.onload = () => {
+    const consentPopup = document.getElementById('consentPopup');
+    const acceptBtn = document.getElementById('accept');
+
+    const acceptFn = event => {
+        saveToStorage(storage);
+        consentPopup.classList.add('hidden');
+    };
+
+    acceptBtn.addEventListener('click', acceptFn);
+
+
+   if(popup(storage)) {
+     consentPopup.classList.remove('hidden');
+   }
+};
+
+
 document.addEventListener("submit" , (event)=>{
     console.log("Validating")
     let user = document.getElementById("user").value;
